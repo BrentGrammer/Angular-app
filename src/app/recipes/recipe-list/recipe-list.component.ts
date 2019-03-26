982b3167f998ec3a417712379adca31d7ccf5097
BrentGrammer <brentmarquez@gmail.com>
2019-03-25 18:30:58 -0600

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'test description', 'https://cdn.pixabay.com/photo/2016/01/03/17/59/bananas-1119790__340.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
