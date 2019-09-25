f21e045f07d5221a197fa8813c2292fa60d56d89
Brent Marquez <brentonmarquez@gmail.com>
2019-09-24 22:02:44 -0600

    set up child routes on recipes component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: "app-recipe-detail",
@@ -8,11 +9,22 @@ import { RecipeService } from "../recipe.service";
   styleUrls: ["./recipe-detail.component.css"]
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // subscribe instead of use the snapshot because the id changes dynamically and the same component is loaded for the
+    // route - Angular will not update the component because of default efficiency behavior without subscribing
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
