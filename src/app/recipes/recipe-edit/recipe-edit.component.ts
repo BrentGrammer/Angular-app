94caf6008cf245365f4b478467957e1c607ad4c7
Brent Marquez <brentonmarquez@gmail.com>
2019-11-06 21:40:47 -0700

    FORM - add validation to recipe edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
-import { FormGroup, FormControl, FormArray } from "@angular/forms";
+import { FormGroup, FormControl, FormArray, Validators } from "@angular/forms";
 import { RecipeService } from "../recipe.service";
 
 @Component({
@@ -27,6 +27,29 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  // This getter is needed in Angular 8 to get accesses to the controls to loop through on the ingredients form controls in the html
+  get controls() {
+    return (<FormArray>this.recipeForm.get("ingredients")).controls;
+  }
+
+  // This method adds a new form group of fields for inputting the name and amount of a new ingredient to add to the recipe
+  // there is a ngFor loop in the formgroup in the html that renders form group fields for each item in the form array in the
+  // form object ingredients property.  This pushes a new empty form group onto that array to generate inputs in the html
+  // note that the button in the html receives the type of button to prevent form submission when this is called on click
+  onAddIngredient() {
+    // need to cast it to FormArray since Typescript doesn't know what type it is
+    // This is done so you can call push on the form array
+    (<FormArray>this.recipeForm.get("ingredients")).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
+      })
+    );
+  }
+
   // Determine whether to populate the form for editing or for creating a new entry based on the edit mode
   // Call this whenever the route params change (in subscription callback to route.params above) which indicates the page is reloaded
   private initForm() {
@@ -48,8 +71,11 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           // use FormGroup since there is name and amount controls to set for the ingredient entry - a group of inputs
           const recipeIngredient = new FormGroup({
-            name: new FormControl(ingredient.name),
-            amount: new FormControl(ingredient.amount)
+            name: new FormControl(ingredient.name, Validators.required),
+            amount: new FormControl(ingredient.amount, [
+              Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/) // only positive nums
+            ])
           });
 
           recipeIngredients.push(recipeIngredient);
@@ -57,29 +83,13 @@ export class RecipeEditComponent implements OnInit {
       }
     }
 
+    // Add validation here to controls
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
       ingredients: recipeIngredients // is a form array initialized above
     });
-  }
-
-  // This method adds a new form group of fields for inputting the name and amount of a new ingredient to add to the recipe
-  // there is a ngFor loop in the formgroup in the html that renders form group fields for each item in the form array in the
-  // form object ingredients property.  This pushes a new empty form group onto that array to generate inputs in the html
-  // note that the button in the html receives the type of button to prevent form submission when this is called on click
-  onAddIngredient() {
-    // need to cast it to FormArray since Typescript doesn't know what type it is
-    // This is done so you can call push on the form array
-    (<FormArray>this.recipeForm.get("ingredients")).push(
-      new FormGroup({ name: new FormControl(), amount: new FormControl() })
-    );
-  }
-
-  // This getter is needed in Angular 8 to get accesses to the controls to loop through on the ingredients form controls in the html
-  get controls() {
-    return (<FormArray>this.recipeForm.get("ingredients")).controls;
   }
 
   onSubmit() {
