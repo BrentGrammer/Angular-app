dd0354ee33564d55bfaf5f08c561dcaab1392b7b
Brent Marquez <brentonmarquez@gmail.com>
2019-11-20 20:58:16 -0700

    add guard for using loaded data instead of refetching if present

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -6,6 +6,7 @@ import {
 } from "@angular/router";
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 /**
  * This resolver ensures that when a page loads it has data that it depends on.
@@ -19,10 +20,21 @@ import { DataStorageService } from "../shared/data-storage.service";
 
 @Injectable({ providedIn: "root" }) // Generic takes type that is returned
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipesService: RecipeService
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    // NOte: no subscribe() call is necessary here because the resolver will subscribe automatically to find out once data is present
-    return this.dataStorageService.fetchRecipes();
+    // Check to see if there are already recipes to use, so that the resolver does not overwrite recipe changes with fetched recipes
+    // only fetch if no recipes are loaded:
+    const recipes = this.recipesService.getRecipes();
+    if (recipes.length === 0) {
+      // NOte: no subscribe() call is necessary here because the resolver will subscribe automatically to find out once data is present
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      // if recipes are loaded, then just return the recipes and don't fetch data
+      return recipes;
+    }
   }
 }
