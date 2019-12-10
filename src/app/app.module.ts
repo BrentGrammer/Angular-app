5e600a281ec57790342a3169c07b5dbda76b24c9
Brent Marquez <brentonmarquez@gmail.com>
2019-12-09 21:53:18 -0700

    add auth template for sign in and sign up and add to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,11 @@ import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeService } from "./recipes/recipe.service";
 import { DataStorageService } from "./shared/data-storage.service";
+import { AuthComponent } from "./auth/auth.component";
 
 @NgModule({
   declarations: [
+    AuthComponent,
     AppComponent,
     HeaderComponent,
     RecipesComponent,
